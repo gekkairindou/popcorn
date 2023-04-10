5340cce8515b3dca63e92f658a7933e7d4dc4390
niizuki <turyuiyokan@gmail.com>
2023-04-11 01:21:46 +0900

    first commit

---
+++ src/main/java/com/shrimp/popcorn/PopcornApplication.java
@@ -0,0 +1,13 @@
+package com.shrimp.popcorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrimp/popcorn/PopcornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrimp.popcorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
